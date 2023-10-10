23b6ab45ab02e13c774417d00d312107d90b2410
Deaath65 <deaath65@gmail.com>
2023-10-10 10:07:00 +0545

    agora conf

---
+++ static/js/streams.js
@@ -0,0 +1,5 @@
+const APP_ID = '3d45f1402ea04215b6fae5ac1983deab'
+const CHANNEL = 'main'
+const TOKEN = '007eJxTYGgxLC3q8D100vHmYWUjpRfr7y93MexltD00IyLqaIS/OYMCg3GKiWmaoYmBUWqigYmRoWmSWVpiqmlisqGlhXFKamLS/PMqqQ2BjAwBV7+wMjJAIIjPwpCbmJnHwAAACQ0fEA=='
+
+console.log('streams.js connected')
