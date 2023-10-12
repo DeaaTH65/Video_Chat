096d8c399e3c833fe7c9a8bbdf207bda4a4f4d40
Deaath65 <deaath65@gmail.com>
2023-10-12 09:08:44 +0545

    camera off

--- static/js/streams.js
+++ static/js/streams.js
@@ -66,5 +66,16 @@ let leaveAndRemoveLocalStream = async () => {
 
 }
 
+let toggleCamera = async (e) => {
+    if(localTracks[1].muted){
+        await localTracks[1].setMuted(false)
+        e.target.style.backgroundColor = '#fff'
+    }else{
+        await localTracks[1].setMuted(true)
+        e.target.style.backgroundColor = 'rgb(255, 80, 80, 1)'
+    }
+}
+
 joinAndDisplayLocalStream()
 document.getElementById('leave-btn').addEventListener('click', leaveAndRemoveLocalStream)
+document.getElementById('vid-btn').addEventListener('click', toggleCamera)
